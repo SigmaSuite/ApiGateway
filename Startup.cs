960f4a9715d6b0ff1616d38330f9ead3fd5ad978
Bartosz Sendek <bartosz.sendek@pl.abb.com>
2021-10-14 21:53:12 +0200

    prepare for integration with authentication service- add ocelot path and JWT token setup

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -17,13 +20,34 @@ namespace ApiGateway
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration= configuration;
-            
+            Configuration = configuration;
+
         }
-        public IConfiguration Configuration{get;}
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            //TODO integrate with authentication service; share secret
+            var key="123";
+
             services.AddControllers();
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer("sigma_suite_api", x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             services.AddOcelot(Configuration);
             services.AddSwaggerGen();
         }
